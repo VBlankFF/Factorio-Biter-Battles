7753080c3ef4a1d5f209d7db5dc17bbc9e226c34
VBlankFF <vblankff@gmail.com>
2025-05-03 08:35:12 -0400

    fiddling with the panel until it does something

--- comfy_panel/special_games/bias_biter_sends.lua
+++ comfy_panel/special_games/bias_biter_sends.lua
@@ -5,27 +5,24 @@ local Public = {
         tooltip = "I'm testing!! Don't look yet!",
     },
     config = {
-        [1] = {name = "bias_random", type = "checkbox", caption = "Random", state = true},
-        [2] = {name = "bias_main", type = "checkbox", caption = "Main", state = false},
-        [3] = {name = "bias_right", type = "checkbox", caption = "Right", state = false },
-        [4] = {name = "bias_left", type = "checkbox", caption = "Left", state = false}
+        [1] = {name = "bias_main", type = "checkbox", caption = "Main", state = false},
+        [2] = {name = "bias_right", type = "checkbox", caption = "Right", state = false },
+        [3] = {name = "bias_left", type = "checkbox", caption = "Left", state = false}
     },
     button = {name = "apply", type = "button", caption = "Apply"},
     generate = function (config, player)
-        if config["bias_random"].state == true then
-            storage.active_special_games['bias_biter_sends'] = false
-        end
         if config["bias_main"].state == true then
             storage.active_special_games['bias_biter_sends'] = true
             storage.special_games_variables['biter_bias'] = 'Main'
-        end
-        if config["bias_right"].state == true then
+        elseif config["bias_right"].state == true then
             storage.active_special_games['bias_biter_sends'] = true
             storage.special_games_variables['biter_bias'] = 'Right'
-        end
-        if config["bias_left"].state == true then
+        elseif config["bias_left"].state == true then
             storage.active_special_games['bias_biter_sends'] = true
             storage.special_games_variables['biter_bias'] = 'Left'
+        else
+            storage.active_special_games['bias_biter_sends'] = false
+            storage.special_games_variables['biter_bias'] = nil
         end
         game.print("biter bias:", storage.active_special_games["bias_biter_sends"])
     end,
