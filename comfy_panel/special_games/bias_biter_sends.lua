7ad08c387392e7e7ff9bdbb8c45b709c6ec829a8
VBlankFF <vblankff@gmail.com>
2025-05-04 10:38:39 -0400

    list now modified when tag is moved or removed, alerts chat

--- comfy_panel/special_games/bias_biter_sends.lua
+++ comfy_panel/special_games/bias_biter_sends.lua
@@ -6,7 +6,6 @@ local function generate_bias_biter_sends(states)
         storage.active_special_games['bias_biter_sends'] = false
         storage.special_games_variables['biter_targets'] = {}
     end
-    game.print({"yippie"})
 end
 
 local function player_added_tag(event)
@@ -18,20 +17,70 @@ local function player_added_tag(event)
     local positions_table = storage.special_games_variables['biter_targets']
     if tag.valid then
         if tag.position.y < 0 and force == "south" then
-            table.insert(positions_table, { position = tag.position })
+            table.insert(positions_table, { tag = tag, position = tag.position })
+            if event.player_index then
+                event.force.print(game.connected_players[event.player_index].name ..
+                " has added a tag at [gps=" .. tag.position.x .. "," .. tag.position.y .. "," .. tag.surface.name .. "]")
+            end
         elseif tag.position.y > 0 and force == "north" then
-            table.insert(positions_table, { position = tag.position })
+            table.insert(positions_table, { tag = tag, position = tag.position })
+            if event.player_index then
+                event.force.print(game.connected_players[event.player_index].name ..
+                " has added a tag at [gps=" .. tag.position.x .. "," .. tag.position.y .. "," .. tag.surface.name .. "]")
+            end
+        end
+    end
+end
+
+local function player_modified_tag(event)
+    if not storage.active_special_games['bias_biter_sends'] then
+        return
+    end
+    local tag = event.tag
+    for i, v in pairs(storage.special_games_variables['biter_targets']) do
+        if v.tag == tag and v.tag.valid then
+            v.position = tag.position
+            if event.player_index and (event.old_position.x ~= tag.position.x or event.old_position.y ~= tag.position.y) then
+                event.force.print(game.connected_players[event.player_index].name ..
+                    " has moved a tag from [gps=" ..
+                    event.old_position.x ..
+                    "," ..
+                    event.old_position.y ..
+                    "," ..
+                    tag.surface.name ..
+                    "] to [gps=" .. tag.position.x .. "," .. tag.position.y .. "," .. tag.surface.name .. "]")
+            end
+            return
+        end
+    end
+end
+
+local function player_removed_tag(event)
+    if not storage.active_special_games['bias_biter_sends'] then
+        return
+    end
+    local tag = event.tag
+    for i, v in pairs(storage.special_games_variables['biter_targets']) do
+        if v.tag == tag and v.tag.valid then
+            table.remove(storage.special_games_variables['biter_targets'], i)
+            if event.player_index then
+                event.force.print(game.connected_players[event.player_index].name ..
+                " has removed a tag from [gps=" .. tag.position.x .. "," .. tag.position.y .. "," .. tag.surface.name .. "]")
+            end
+            return
         end
     end
 end
 
 script.on_event(defines.events.on_chart_tag_added, player_added_tag)
+script.on_event(defines.events.on_chart_tag_modified, player_modified_tag)
+script.on_event(defines.events.on_chart_tag_removed, player_removed_tag)
 
 local Public = {
     name = {
         type = "label",
-        caption = "Bias Biter Sends",
-        tooltip = "I'm testing!! Don't look yet!",
+        caption = "Biter Targeting",
+        tooltip = "Biters will target map tags placed on the opponent's side of the map",
     },
     config = {
         [1] = {name = "enable", type = "checkbox", caption = "Enable", state = false},
